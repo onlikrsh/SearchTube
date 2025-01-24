ce976e3f8619c28864c5c6838d218e057b54c959
onlikrsh <kharshith920@gmail.com>
2025-01-24 17:47:43 +0000

    Fix Buttons

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const Input = document.getElementById('searchInput');
-const bonusIcon = document.getElementById('leftIcon');
+// const bonusIcon = document.getElementById('leftIcon');
 
 function clearSearch() {
     Input.value = '';
     Input.focus();
 }
 
-Input.addEventListener('focus', () => {
-    Input.style.width = '400px'; 
-    Input.style.transition = 'width 0.3s ease';
-    bonusIcon.style.display = 'flex'; 
-});
+// Input.addEventListener('focus', () => {
+//     Input.style.width = '400px'; 
+//     Input.style.transition = 'width 0.3s ease';
+//     bonusIcon.style.display = 'flex'; 
+// });
   
-Input.addEventListener('blur', () => {
-    Input.style.width = '340px'; 
-    bonusIcon.style.display = 'none';
-});
+// Input.addEventListener('blur', () => {
+//     Input.style.width = '340px'; 
+//     bonusIcon.style.display = 'none';
+// });
 
